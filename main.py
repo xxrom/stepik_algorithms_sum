54cbeb037153cd8f826c20265b09fa1f5a76280c
Chernyshov <xpom55@mail.ru>
2018-05-15 19:23:07 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+  a, b = map(int, input().split())
+  res = a + b
+  print(res)
+
+
+if __name__ == "__main__":
+  main()
+
+# print(input())
+# print(input().split())
+# a, b = map(int, input().split())
+# array = input().split()
+# res = a + b
+# print(res)
